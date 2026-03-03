d5d7c5c7552cd64dc32ddb05fab88b2889ba62c5
Magan Greenfield <magan.wagner@gmail.com>
2026-03-03 14:33:22 -0600

    new project started

---
+++ week2/FileProcessing/file_reader.py
@@ -0,0 +1,13 @@
+def read_csv_file(filepath):
+    """
+    Read a CSV file and return a list of dictionaries.
+    
+    Should handle:
+    - FileNotFoundError
+    - UnicodeDecodeError (try utf-8, then latin-1)
+    - Empty files
+    
+    Returns: List of dictionaries (one per row)
+    Raises: FileProcessingError with descriptive message
+    """
+    pass
